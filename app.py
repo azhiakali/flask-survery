4330834b3e1e648aa2cc6a096bc8e7255b49371d
Azhia-Kauba <azhia.kali@gmail.com>
2022-05-19 18:09:24 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, render_template, redirect, flash
+from random import randint, choice, sample
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "oh-so-secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
